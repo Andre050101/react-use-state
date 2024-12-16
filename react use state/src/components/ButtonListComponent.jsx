fdcece97bf47609303532b5585b173824230e45a
Andre050101 <agiancristiano90@gmail.com>
2024-12-16 17:11:25 +0100

    ButtonListComponent created(not stylized yet)

--- react use state/src/components/ButtonListComponent.jsx
+++ react use state/src/components/ButtonListComponent.jsx
@@ -1,2 +1,16 @@
 import React from "react";
 import styles from "./ButtonListComponent.module.css";
+
+function ButtonListComponent({ languages, selectedLanguage, onSelectLanguage }) {
+    return (
+        <div>
+            {languages.map((language) => (
+                <button key={language.id} onClick={() => onSelectLanguage(language)}>
+                    {language.title}
+                </button>
+            ))}
+        </div>
+    );
+}
+
+export default ButtonListComponent;
