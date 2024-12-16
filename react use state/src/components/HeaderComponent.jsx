50303e5abe96ee59a95fea97f22ea8a44b6df4a3
Andre050101 <agiancristiano90@gmail.com>
2024-12-16 16:56:11 +0100

    header done

---
+++ react use state/src/components/HeaderComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./HeaderComponent.module.css";
+
+function HeaderComponent() {
+    return (
+        <header className={styles.header}>
+            <h1>Learn Web Development</h1>
+        </header>
+    );
+}
+
+export default HeaderComponent;
